eb85ddeee1947232ce624c625617a80bedcef855
Sameerkhan344 <khansame34@gmail.com>
2023-11-06 09:21:30 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Home from "@/Components/Home/Home";
+import Workpage from "@/Components/Workpage/Workpage";
+import Productspage from "@/Components/Productspage/Productspage";
+import Plateformpage from "@/Components/Plateformpage/Plateformpage";
+import Footer from "@/Components/Footer/Footer";
+import Header from "@/Components/Header/Header";
+const page = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Workpage />
+      <Productspage />
+      <Plateformpage />
+      <Footer />
+    </>
+  );
+};
+
+export default page;
